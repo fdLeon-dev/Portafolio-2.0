f45f3bbb13b22afab143caa4d2d2720a80a74784
Facundo de leon <64671402+fdLeon-dev@users.noreply.github.com>
2025-10-03 02:33:15 -0300

    cambios

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-document.getElementById('year').textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById('year').textContent = new Date().getFullYear();
 
-// Toggle menú mobile
-const toggle = document.getElementById('menuToggle');
-const nav = document.getElementById('navList');
+  // Toggle menú mobile
+  const toggle = document.getElementById('menuToggle');
+  const nav = document.getElementById('navList');
 
-if (toggle) {
-  toggle.addEventListener('click', () => {
-    if (nav.style.display === 'flex') nav.style.display = 'none';
-    else nav.style.display = 'flex';
-    nav.style.flexDirection = 'column';
-    nav.style.gap = '10px';
-    nav.style.background = 'rgba(11,8,25,0.95)';
-    nav.style.position = 'absolute';
-    nav.style.right = '20px';
-    nav.style.top = '64px';
-    nav.style.padding = '12px';
-    nav.style.borderRadius = '12px';
-  });
-}
+  if (toggle) {
+    toggle.addEventListener('click', () => {
+      if (nav.style.display === 'flex') nav.style.display = 'none';
+      else nav.style.display = 'flex';
+      nav.style.flexDirection = 'column';
+      nav.style.gap = '10px';
+      nav.style.background = 'rgba(11,8,25,0.95)';
+      nav.style.position = 'absolute';
+      nav.style.right = '20px';
+      nav.style.top = '64px';
+      nav.style.padding = '12px';
+      nav.style.borderRadius = '12px';
+    });
+  }
+});
 
 // Animaciones en scroll
 const faders = document.querySelectorAll('.fade-in');
@@ -57,4 +59,26 @@ form.addEventListener("submit", function (e) {
     formMessage.textContent = "✅ ¡Mensaje enviado con éxito!";
     form.reset();
   }, 2000);
+});
+
+const navLinks = document.querySelectorAll("nav a");
+const sections = document.querySelectorAll("main .section");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    // quitar activo del nav
+    navLinks.forEach(nav => nav.classList.remove("active"));
+    link.classList.add("active");
+
+    // ocultar todas las secciones
+    sections.forEach(sec => sec.classList.remove("active"));
+
+    // mostrar la elegida
+    const target = document.getElementById(link.dataset.section);
+    if (target) {
+      target.classList.add("active");
+    }
+  });
 });
