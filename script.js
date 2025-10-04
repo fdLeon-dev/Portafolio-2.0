981d1c7590e19792b834275ad083b4d5cbb105db
Facundo de leon <64671402+fdLeon-dev@users.noreply.github.com>
2025-10-03 23:58:02 -0300

    Responsive

--- script.js
+++ script.js
@@ -20,18 +20,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
-// Animaciones en scroll
 const faders = document.querySelectorAll('.fade-in');
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries, obs) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
+      obs.unobserve(entry.target);
     }
   });
 }, { threshold: 0.2 });
 
-faders.forEach(fader => observer.observe(fader));
+faders.forEach(f => observer.observe(f));
 
 
 // Animar barras al cargar
